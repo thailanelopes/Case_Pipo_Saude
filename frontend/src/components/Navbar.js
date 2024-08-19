51cab7f8bad9719ce3f5917297e3ff76a3edf3d7
thailanelopes <thailane111122@gmail.com>
2024-08-19 20:01:49 -0300

    Subindo ajustes styles

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -3,7 +3,7 @@ import AppBar from '@mui/material/AppBar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
-import logo from '../logo.jpg';
+import logo from '../logoarr.png';
 import styles from './Navbar.module.css';
 
 const Navbar = () => {
--- frontend/src/styles/theme.js
+++ frontend/src/styles/theme.js
@@ -3,7 +3,7 @@ import { createTheme } from '@mui/material/styles';
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#0000ff',
+      main: '#2626c1',
     },
     secondary: {
       main: '#ffffff', 
